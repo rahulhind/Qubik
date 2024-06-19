618890d7b010d51f430df26ca59505d0e2ae5074
Rahul Kumar <username78588@gmail.com>
2024-06-19 07:34:46 +0530

    Fixes

--- pages/utils/agoraHelper.ts
+++
@@ -1,5 +0,0 @@
-// import { Message } from ".././types/Message";
-
-// export function convertToYouThem(message: Message, userId: string) {
-//   return message.userId === userId ? "You" : "Them";
-// }
